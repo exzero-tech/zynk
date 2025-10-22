4cbc91063101c2066754dfbdf04c635d0b675b8e
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2025-10-22 16:07:46 +0530

    feat: implement the basic color and font themes

--- driver-app/app/_layout.tsx
+++ driver-app/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -11,6 +12,18 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const [fontsLoaded] = useFonts({
+    'Inter_24pt-Light': require('../assets/fonts/Inter_24pt-Light.ttf'),
+    'Inter_24pt-Regular': require('../assets/fonts/Inter_24pt-Regular.ttf'),
+    'Inter_24pt-Medium': require('../assets/fonts/Inter_24pt-Medium.ttf'),
+    'Inter_24pt-SemiBold': require('../assets/fonts/Inter_24pt-SemiBold.ttf'),
+    'Inter_24pt-Bold': require('../assets/fonts/Inter_24pt-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null; // Or a loading screen
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
